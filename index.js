9b0982599ba864e51eff0470690364cb16b36bef
slowpoke1025 <slowpoke901025@gmail.com>
2024-11-26 16:52:13 +0800

    test

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const PORT = process.env.PORT || 3000;
 
 
 const list = [
-  "https://cdnfinance.technews.tw/page/2/",
-  "https://cdnfinance.technews.tw/page/3/",
-  "https://cdnfinance.technews.tw/page/4/",
-  "https://cdnfinance.technews.tw/page/5/"
+  "https://tw.portal-pokemon.com/play/pokedex/0001",
+  "https://tw.portal-pokemon.com/play/pokedex/0004",
+  "https://tw.portal-pokemon.com/play/pokedex/0007",
+  "https://tw.portal-pokemon.com/play/pokedex/0025"
 ]
 
 let i = -1;
@@ -20,14 +20,19 @@ let i = -1;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.ip} ${req.url}`);
+  next();
+});
 
 
-app.get('/test', (req, res) => {
+
+app.get('/', (req, res) => {
   i++;
   const redirectUrl = list[i % list.length];
   
   res.render('redirect', { redirectUrl,  i:i % list.length});
-
+  console.log(`click = ${i+1}`)
 });
 
 
