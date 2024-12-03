54356f68873b9177da9cbbf9c1175b7f7ddcfbbb
slowpoke1025 <slowpoke901025@gmail.com>
2024-12-04 00:39:53 +0800

    dashboard and mongodb

--- index3.js
+++ index3.js
@@ -13,12 +13,35 @@ const list = [
   { url: "https://www.surveycake.com/s/og7N2", status: "active" },
 ];
 
-// const clicks = [0, 0, 0, 0];
+const mongoose = require("mongoose");
+const uri =
+  "mongodb+srv://slowpoke:ZyjFXQIU1pq2uOee@scrapy.dhxbc.mongodb.net/?retryWrites=true&w=majority&appName=Scrapy";
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    const db = mongoose.connection.useDb("rm");
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err.toString());
+  });
+
+const logSchema = new mongoose.Schema({
+  url: { type: String, required: true },
+  ip: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
+});
+let Log = mongoose.model("Log", logSchema);
+
+mongoose.disconnect().then((d) => {
+  console.log("db close");
+});
 const clicks = {};
 list.forEach((i) => {
   clicks[i.url] = 0;
 });
-let i = -1;
+let i = 0;
 
 // Store logs
 const logs = [];
@@ -42,58 +65,81 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  if (req.url == "/test") {
-    const logEntry = {
-      url: req.url,
-      ip: req.ip,
-      timestamp: new Date(),
-    };
-    logs.push(logEntry); // Save log entry
-  }
-
+app.use(async (req, res, next) => {
   console.log(`\nIncoming request: ${req.url}\n`);
   console.log(`IP: ${req.ip}`);
   next();
 });
 
-app.get("/test", (req, res) => {
+app.get("/test", async (req, res) => {
   let index, redirectUrl;
   const activeList = list.filter((item) => item.status === "active");
 
   if (req.session.url == null) {
-    ++i;
     index = i % activeList.length;
     redirectUrl = activeList[index].url;
     req.session.url = redirectUrl;
     ++clicks[redirectUrl];
+    ++i;
   } else {
     redirectUrl = req.session.url;
+  }
+
+  const logEntry = {
+    url: redirectUrl,
+    ip: req.ip,
+    timestamp: new Date(),
+  };
+  logs.push(logEntry); // Save log entry
+  try {
+    const dbLog = new Log(logEntry);
+    await dbLog.save();
+    console.log("Log saved to MongoDB:", dbLog);
+  } catch (err) {
+    console.error("Error saving log:", err.toString());
   }
 
   res.render("redirect", { redirectUrl });
   console.log(`url: ${redirectUrl}`);
   console.log(`click: ${clicks[redirectUrl]}`);
-  console.log(`total click: ${i + 1}`);
+  console.log(`total click: ${totalClick()}`);
 });
 
-app.get("/clear", (req, res) => {
+app.get("/clear", async (req, res) => {
   for (let j = 0; j < list.length; j++) {
-    clicks[list.url] = 0;
+    clicks[list[j].url] = 0;
   }
   logs.length = 0; // Clear logs
-  i = -1;
-  res.send("Clear");
+  i = 0;
+
+  try {
+    // Delete all documents
+    const result = await Log.deleteMany({});
+    console.log(`${result.deletedCount} logs cleared from the collection.`);
+  } catch (err) {
+    console.error("Error clearing logs:", err.toString());
+  }
+
+  res.redirect("/logs"); // Redirect back to the logs page
 });
 
-app.get("/logs", (req, res) => {
+app.get("/logs", async (req, res) => {
   const linksData = list.map((item, index) => ({
     url: item.url,
     status: item.status,
     clicks: clicks[item.url],
   }));
+  let _logs;
+  try {
+    _logs = await Log.find().sort({ timestamp: -1 }).exec();
+  } catch (err) {
+    console.error("Error fetching logs:", err.toString());
+    _logs = logs;
+  }
+  const activeList = list.filter((item) => item.status === "active");
 
-  res.render("logs3", { logs, linksData });
+  let nextLink = activeList[i % activeList.length].url;
+  res.render("logs3", { logs: _logs, linksData, nextLink });
 });
 
 app.post(
@@ -116,6 +162,10 @@ app.post(
     res.redirect("/logs"); // Redirect back to the logs page
   }
 );
+
+function totalClick() {
+  return Object.values(clicks).reduce((sum, count) => sum + count);
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
